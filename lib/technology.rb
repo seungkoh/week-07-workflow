dd09800c301eefe8a0eb3aa17e66e4242e88d34a
Mike Koh <skoh@uw.edu[color]>
2011-11-21 05:10:35 -0800

    Workflow with additional states and specs to cover new states.

--- lib/technology.rb
+++ lib/technology.rb
@@ -8,19 +8,32 @@ require 'workflow'
 class Technology
   include Workflow
   workflow do
+    state :new do
+      event :submit, :transitions_to => :unapproved
+    end
     state :unapproved do
       event :approve, :transitions_to => :approved
+      event :reject, :transitions_to => :rejected
     end
     state :approved do
       event :publish, :transitions_to => :published
       event :unapprove, :transitions_to => :unapproved
     end
     state :published do
+      event :revise, :transitions_to => :revised
       event :retire,  :transitions_to => :retired
     end
+    state :revised do
+      event :submit, :transitions_to => :unapproved
+    end
     state :retired
+    state :rejected
   end
  
+  def submit
+    puts 'technology is submitted'
+  end
+  
   def approve
     puts 'technology is approved'
     # send an email or log to a file
@@ -34,8 +47,15 @@ class Technology
     puts 'technology is published'
   end
   
+  def revise
+    puts 'technology is revised'
+  end
+  
   def retire
     puts 'technology is retired'
   end
   
+  def reject
+    puts 'technology is rejected'
+  end
 end
--- spec/technology_spec.rb
+++ spec/technology_spec.rb
@@ -8,28 +8,42 @@ describe "states" do
 
   subject { Technology.new } 
 
-  it "should start unapproved" do
-    subject.current_state.to_s.should == "unapproved"
+  it "should start new" do
+    subject.current_state.to_s.should == "new"
+  end
+
+  # I moved this out of the "when approved" context
+  context "when unapproved" do
+    it "should not be publishable" do
+      subject.submit!
+      expect { subject.publish! }.to raise_error
+    end
+    it "can be rejected" do
+      subject.submit!
+      subject.current_state.events.should have_key(:reject)
+      subject.current_state.events.keys.should include(:reject)
+    end
   end
 
   context "when approved" do
     it "should be approved" do
+      subject.submit!
       subject.approve!
       subject.approved?.should == true
       subject.current_state.to_s.should == "approved"
     end
-    it "should not be publishable when unapproved" do
-      expect { subject.publish! }.to raise_error
-    end
     it "should tell us it is approved" do
-      expect { subject.approve!}.to_s == "technology is approved"
+      subject.submit!
+      expect { subject.approve! }.to_s == "technology is approved"
     end
     it "can be set back to unapproved" do
+      subject.submit!
       subject.approve!
       subject.unapprove!
       subject.current_state.to_s.should == "unapproved"
     end
     it "available events should be publish and unapprove" do
+      subject.submit!
       subject.approve!
       subject.current_state.events.should have_key(:publish)
       subject.current_state.events.should have_key(:unapprove)
@@ -38,12 +52,68 @@ describe "states" do
   end
 
   context "when published" do
-    it "should be be published" do
+    it "should be published" do
+      subject.submit!
       subject.approve!
       subject.publish!
       subject.current_state.to_s.should == "published"
     end
+    it "available events should be retire and revise" do
+      subject.submit!
+      subject.approve!
+      subject.publish!
+      subject.current_state.events.should have_key(:retire)
+      subject.current_state.events.should have_key(:revise)
+      subject.current_state.events.keys.should == [:revise, :retire]
+    end
+  end
+  
+  context "when revised" do
+    it "should be submitted again before approval" do
+      subject.submit!
+      subject.approve!
+      subject.publish!
+      subject.revise!
+      subject.submit!
+      subject.approved?.should == false
+      subject.current_state.to_s.should == "unapproved"
+    end
+    it "should not be immediately publishable" do
+      subject.submit!
+      subject.approve!
+      subject.publish!
+      subject.revise!
+      expect { subject.publish! }.to raise_error      
+    end
   end
 
-  
+  context "when rejected" do
+    it "should be rejected" do
+      subject.submit!
+      subject.reject!
+      subject.current_state.to_s.should == "rejected"
+    end
+    it "should not have any available events" do
+      subject.submit!
+      subject.reject!
+      subject.current_state.events.should be_empty
+    end
+  end
+
+  context "when retired" do
+    it "should be retired" do
+      subject.submit!
+      subject.approve!
+      subject.publish!
+      subject.retire!
+      subject.current_state.to_s.should == "retired"
+    end
+    it "should not have any available events" do
+      subject.submit!
+      subject.approve!
+      subject.publish!
+      subject.retire!
+      subject.current_state.events.should be_empty
+    end
+  end  
 end
